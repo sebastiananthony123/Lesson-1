91e6bbd0356d5a08506c7cb6290013bfe439e951
sebastiananthony123 <tm@solumre.com>
2024-10-03 17:58:27 +0100

    conditions

---
+++ conditionals.py
@@ -0,0 +1,12 @@
+x = int(input("Enter a number: "))
+
+if x < 0:
+    print(f"{x} is a negative number")
+
+elif x > 0:
+    print(f"{x} is a positive number")
+
+else:
+    print(f"{x} is a neutral number")
+
+
